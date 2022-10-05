49fc1ead0cfecad97e16652e60684dba549aecd3
Martina Schories <20225984+mschories@users.noreply.github.com>
2022-10-05 13:56:48 +0200

    Update config.R

--- config/config.R
+++ config/config.R
@@ -3,3 +3,4 @@ DATAPATH_RAW_DE <- "https://jupyter.digitalmethodologies.center/user/b03/files/d
 DATAPATH_RAW_WORLD <- "https://jupyter.digitalmethodologies.center/user/b03/files/data/World/html-file-information.csv?token="
 DATAPATH_RAW_NL <- "https://jupyter.digitalmethodologies.center/user/b03/files/data/Dutch/html-file-information.csv?token="
 
+path_to_shinydata <- "shiny/b03-commenting-museum/data/"
