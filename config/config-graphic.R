fa85a49db745336421bc23f62736bc1cd387aaca
Martina Schories <20225984+mschories@users.noreply.github.com>
2024-07-04 10:03:39 +0200

    Update config-graphic.R

--- config/config-graphic.R
+++ config/config-graphic.R
@@ -1,9 +1,14 @@
 
 library(ggtext)
+library(gdtools)
+# library(extrafont)
+# extrafont::loadfonts(quiet = TRUE)
+gdtools::register_gfont("Roboto Mono")
 
-typo_sfb_serif <- "Nantes-Regular"
-typo_sfb_mono <- "GTAmericaMonoLC-Rg"
+typo_sfb_mono <- "GT America Mono LC Rg"
+# typo_sfb_mono_bold <- "Roboto Mono"
 
+# systemfonts::register_font(typo_sfb_mono)
 # typo_sfb_mono_remote <- "GT America Mono LC Rg"
 # typo_sfb_mono_bold_remote <- "GT America Mono Rg"
 
@@ -53,9 +58,24 @@ theme_b03_heatmap_details <- theme(
   )
   
 theme_b03_facets <- theme(
-    strip.text = element_text(family = typo_sfb_mono, size = 13, hjust = 0)
+    strip.text = element_text(family = typo_sfb_mono, size = 11, hjust = 0, angle = 0),
+    panel.grid.major.y = element_blank(),
+    panel.background = element_rect(fill = "#f1f1f1")
   )
-  
+
+theme_b03_facets_individual <- theme(
+  strip.background = element_rect(fill = "#fbfbfb", color = NA),
+  strip.text = element_text(family = typo_sfb_mono, size = 13, hjust = 0, angle=0),
+  panel.grid.major.y = element_line(color = "#e2e2e2", linewidth = .2),
+  strip.placement = "outside"#,
+  # strip.switch.pad.grid = unit(2, "in")
+)
+
+theme_b03_panel_spacing <- theme(
+  panel.spacing.y = unit(.1, "in"),
+  panel.spacing.x = unit(.2, "in")
+)  
+
 theme_b03_text <- theme(
     text = element_text(family = typo_sfb_mono, size = 12, hjust = 0),
     panel.spacing = unit(0, "in")
@@ -73,9 +93,16 @@ theme_b03_legend_discrete <- theme(
   legend.key.width = unit(1, "lines")
 )
   
-  theme_b03_box_timeline <- theme(
+theme_b03_box_timeline <- theme(
     axis.text.y = element_text(size = 8),
     panel.grid.major.x = element_line(color = "#c2c2c2", linewidth = .2),
     panel.grid.minor.x = element_line(color = "#e2e2e2", linewidth = .2)
   )
+
+theme_b03_timeline_faceted <- theme(
+  panel.grid.major.y = element_line(color = "#ffffff", linewidth = .2),
+  panel.grid.minor = element_blank(),
+  panel.grid.minor.x = element_line(color = "#ffffff", linewidth = .2),
+  panel.grid.major.x = element_blank()
+)
   
