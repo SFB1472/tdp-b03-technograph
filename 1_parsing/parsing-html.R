97ee1034ea727354ec053b1430281a287adf187d
Martina Schories <20225984+mschories@users.noreply.github.com>
2025-07-16 16:26:34 +0200

    Fixed bug regarding processes in output, updated info which sites to parse

--- 1_parsing/parsing-html.R
+++ 1_parsing/parsing-html.R
@@ -10,25 +10,59 @@ library(xml2)
 library(urltools)
 library(googlesheets4)
 library(testdat)
+library(DBI)
+
+library(RPostgres)
 library(furrr)
 source("config/config.R")
+source("config/config-secret.R")
 
 future::plan(multisession)
 
 search_date <- lubridate::today()
-html_element_to_search_for <- "iframe"
+html_element_to_search_for <- "script"
+
+
+
+CURRENT_SPHERE <- "Dutch"
+
+# delivery <- "1"
+
+con <- dbConnect(RPostgres::Postgres(), 
+                 dbname = dsn_database,
+                 host = dsn_hostname, 
+                 port = dsn_port,
+                 user = dsn_uid, 
+                 password = dsn_pwd
+)
 
 ## risky lines ahead, cause they can destroy already parsed data
 # df_empty <- tibble(site = character(), tag = character(), search_date = date(), name = character(), attr = character(), group = character(), missing = character())
 # write_csv(df_empty, paste0("data/1-parsing/tags/", CURRENT_SPHERE, "/", html_element_to_search_for, "-raw.csv"))
 
-file_list <- list.files(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/")) #%>% head(20)
+file_list <- dbGetQuery(conn = con, paste0("SELECT s.sha1 FROM sites s WHERE s.sphere = '", CURRENT_SPHERE, "' AND s.of_interest = TRUE")) %>% 
+  distinct() %>% 
+  # mutate(sha1 = paste0(sha1, ".html")) %>% 
+  select(sha1) %>% pull()
+
+# file_list <- list.files(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-", delivery, "/")) #%>% head(20)
+# 
+# already_parsed_sites <- list.files(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/")) #%>% head(20)
+# file_list <- setdiff(html_files_of_interest, already_parsed_sites)
+broken_sites <- c("48ddbc87878a63872636bd7188adea4076215989", "da6a398b5265df86a1bc482d234d4833485bdcea", "ef8c8a4c25c01b1a35b73ee3466522e9fa1779d6", 
+                  "798cfff45a6e563d3f93b2b70f4aa6f13815de19", "c62b0ce32e34fc1a1f0fd4d5d8075a96c2a13bac", "0f038852a28befd8d23e04439a175e12a95bcf4f", 
+                  "af2eaa296d92bb92742095ac43a729fa4858de46", "8b56602a79bc7516e115f19497487dfbc0946130", "17426cebf9a8a68e2939302812bccabbbe577e81",
+                  "e288b30d705423f91dcf26f83023a9569e698b28", "b5fd8fbb96a19685215a44e28725eaa73f40ce7d", "052e8349087ae241a77cdf0bfe72c6b2fae71bd0",
+                  "3bdd3fb99c037131ff38aeee87a9c34bf9265d89", "366ccf96502c8a869e08cecd324ff1e257591dbf", "08d19979c9159a59870b954d4c0dd09663873ff4",
+                  "fcd033c5916d2270fe8b0e83b4bd202f2506a08e", "e7ad8cdaacc4204caca82d50dfe6c88a8dc3755f"
+                  )
+file_list <- file_list[!file_list %in% broken_sites]
+
+rm(tags)
 
 # parsing tags ------------------------------------------------------------
 
-df_tags <- future_map(file_list, function(i){
-# df_tags <- map(file_list, function(i){
-  
+tags <- future_map(file_list, function(i){
   # i <- "0480ddd7f304507a4f7fb18bf5ae39fe2e994a29.html"
 
   # cli::cli_inform("Parse file {i}")
@@ -43,9 +77,15 @@ df_tags <- future_map(file_list, function(i){
     missing = "NA"
   )
   
-  site <- xml2::read_xml(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/", i), as_html = TRUE, encoding = "UTF-16", options = c("RECOVER", "HUGE")) #%>%
-  # site <- xml2::read_xml(paste0("data/0-preprocessing/", CURRENT_SPHERE, "/", i), as_html = TRUE, encoding = "UTF-16", options = c("RECOVER", "HUGE")) #%>%
+  # site <- xml2::read_xml(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-",delivery,"/", i), as_html = TRUE, encoding = "UTF-16", options = c("RECOVER", "HUGE")) #%>%
   
+  if(file.exists(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-1/", i, ".html"))){
+    # print("file is da")
+    site <- xml2::read_xml(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-1/", i,".html"), as_html = TRUE, encoding = "UTF-16", options = c("RECOVER", "HUGE"))
+  }else{
+    # print("file is nich da")
+    site <- xml2::read_xml(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/", i,".html"), as_html = TRUE, encoding = "UTF-16", options = c("RECOVER", "HUGE"))
+  }
   
   if(length(site) > 1){
     all_tags <- site %>% html_elements(html_element_to_search_for)
@@ -76,34 +116,46 @@ df_tags <- future_map(file_list, function(i){
         unnest(attrs) %>%
         unnest(data) %>%
         mutate(missing = "NA")
-      
-      # print(df_inner)
-      
-      test_that(
-        desc = "df_inner: no duplicates",
-        expect_unique(everything(), data = df_inner)
-      )
+
     } else{
-      print("no tags found")
+      # print("no tags found")
       df_empty$missing[[1]] = "no tags found"
       df_inner <- df_empty
     }
   } else {
-    print("unvalid html site")
+    # print("unvalid html site")
     df_empty$missing[[1]] = "unvalid html site"
     df_inner <- df_empty
   }
-  # print(df_inner)
-  # print(paste0("before saving: ", "data/1-parsing/tags/", CURRENT_SPHERE, "/", html_element_to_search_for, "-raw-2-2.csv"))
-  write_csv(df_inner, paste0("data/1-parsing/tags/", CURRENT_SPHERE, "/", html_element_to_search_for, "-raw-2.csv"), append = TRUE)
+  pid = Sys.getpid()
+  write_csv(df_inner, paste0("data/1-parsing/tags/", CURRENT_SPHERE, "/", html_element_to_search_for, "/", html_element_to_search_for, "-raw-", pid, ".csv"), append = TRUE)
+ 
   
 }, .progress = TRUE)
 
-# if parsing the many files fail, restart here ------------------------------------------------------------
-
-already_parsed_sites <- read_csv(paste0("data/1-parsing/tags/", CURRENT_SPHERE, "/", html_element_to_search_for,"-raw-2.csv"), col_select = c("site")) %>% #select(site) %>%
-  distinct() %>% mutate(site = paste0(site, ".html")) %>%  pull()
+# if parsing the many files fails, restart here ------------------------------------------------------------
+# load(file="data/helper/file_list_db_de.RData")
+header <- c("site","tag","search_date","attr","value","group","missing","sphere")
+list_of_files <- list.files(path = paste0("data/1-parsing/tags/",CURRENT_SPHERE,"/", html_element_to_search_for,"/"), recursive = FALSE, pattern = "\\.csv$", full.names = TRUE)
+already_parsed_sites <- read_csv(list_of_files, id = "file_name", col_names = header) %>% 
+  select(site) %>%
+  distinct() %>% 
+  # mutate(site = paste0(site, ".html")) %>%
+  pull()
 
 file_list <- setdiff(file_list, already_parsed_sites)
 
 
+# clipr::write_clip(file_list)
+# if future map is finishing on its own, saving the local variable for checking on proper parsing ------------------
+rm(df_tags__)
+df_tags__ <- df_tags %>% 
+  enframe() %>% 
+  # select(value) %>% 
+  unnest(value, names_sep = "_") %>% 
+  as_tibble() %>% 
+  unnest(value_missing, keep_empty = TRUE) %>%
+  rename(id = name) %>% #, str_remove(., "value_"))
+  rename_with(., ~gsub("value_", "", .x))
+
+write_csv(df_tags__, paste0("data/1-parsing/tags/",CURRENT_SPHERE,"/",html_element_to_search_for,"/",html_element_to_search_for,"-raw-",delivery,".csv"))
--- 1_parsing/parsing-snippits.R
+++ 1_parsing/parsing-snippits.R
@@ -4,36 +4,66 @@ library(lubridate)
 library(furrr)
 library(future)
 source("config/config.R")
+library(DBI)
+library(RPostgres)
+# source("config/config-secret.R")
+source("config/config-secret-local.R")
+CURRENT_SPHERE <- "German"
 
+con <- dbConnect(RPostgres::Postgres(), 
+                 dbname = dsn_database,
+                 host = dsn_hostname, 
+                 port = dsn_port,
+                 user = dsn_uid, 
+                 password = dsn_pwd
+)
 future::plan(multisession)
 
 search_date <- lubridate::today()
-file_list <- list.files(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/"))
+# files to parse ---------------------------
+# file_list <- list.files(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/"))
+df_sites <- dbGetQuery(conn = con, paste0("SELECT s.crawl_date, s.sha1, s.export FROM sites s WHERE s.of_interest = TRUE AND s.sphere ='German'")) #%>%
+file_list <- df_sites %>% select(sha1) %>% pull()
 
-snippets_to_search_for <- read_csv("data/helper/23-01-13-Commenting-system-detection-patterns.csv") %>% 
-  select("system" = `Commenting system`, "snippet" = Snippet) %>% 
-  filter(!is.na(system)) %>% select(snippet) %>% pull(.)
+# snippets to search for --------------------
+# snippets_to_search_for <- read_csv("data/helper/23-01-13-Commenting-system-detection-patterns.csv") %>% 
+#   select("system" = `Commenting system`, "snippet" = Snippet) %>% 
+#   filter(!is.na(system)) %>% select(snippet) %>% pull(.)
+
+# load(file="data/helper/tracker-domains.RData")
+# load(file="data/helper/tracker-snippets/tracker-pattern.RData")
+# load(file="data/helper/tracker-snippets/easylist-general-hide.RData")
+load(file = "data/helper/tracker-snippets/all-snippets.RData")
 
 df_snippet_detected <- future_walk(file_list, function(j){
   # print(j)
   # j = "349474870c5d6c02312506f702c5430c25f0e259.html"
   
-  site <- read_file(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/", j))
+  if(file.exists(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-1/", j, ".html"))){
+    # print("file is da")
+    site <- read_file(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-1/", j, ".html"))
+  }else{
+    # print("file is nich da")
+    site <- read_file(paste0("data/0-preprocessing/", CURRENT_SPHERE, "-2/", j, ".html"))
+  }
   
   snippets <- map_df(snippets_to_search_for, function(i){
+  # snippets <- map_df(domains_to_search_for, function(i){
+  # snippets <- map_df(patterns_to_search_for, function(i){
     # print(i)
     found = ifelse(str_detect(site, fixed(i)), 1, 0) ## findet nix!s
-    
-    df_inner <- tibble(
-      site = j %>% str_remove(".html"), 
-      search_date = search_date,
-      snippet = i,
-      detected = found
-    )
+    if(found == 1){
+      df_inner <- tibble(
+        site = j %>% str_remove(".html"), 
+        search_date = search_date,
+        snippet = i,
+        detected = found
+      )
+    }
 
   },.progress = TRUE)
-
-  write_csv(snippets, paste0("data/1-parsing/snippet-detection/", CURRENT_SPHERE,"/snippets-2.csv"), append = TRUE)
+  pid = Sys.getpid()
+  write_csv(snippets, paste0("data/1-parsing/snippet-detection/", CURRENT_SPHERE,"/tracker-all-snippets-", pid, ".csv"), append = TRUE)
 })
 
 #2d83cf0072d3868f5aa4e725d734f24861393483
